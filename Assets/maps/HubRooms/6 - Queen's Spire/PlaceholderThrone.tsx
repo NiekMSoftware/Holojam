b8779cdade2ae2f202f8d3f4b943bb7bcb727ba1
ZombieManF <zombiemanf10@gmail.com>
2024-10-29 17:43:17 -0400

    Throne added kinda

---
+++ Assets/maps/HubRooms/6 - Queen's Spire/PlaceholderThrone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Throne" tilewidth="16" tileheight="16" tilecount="143" columns="11">
+ <image source="PlaceholderThrone.png" width="176" height="208"/>
+</tileset>
