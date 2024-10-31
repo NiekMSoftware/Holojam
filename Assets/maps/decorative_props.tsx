ddd66f21dc145038d96b70ca2e5c2177013eca97
DrDoak <luoleizhao2018@u.northwestern.com>
2024-10-30 22:50:24 -0400

    decorative prop tsx

---
+++ Assets/maps/decorative_props.tsx
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="decorative_props" tilewidth="16" tileheight="16" tilecount="128" columns="16">
+ <image source="../Tiles/decorative_props.png" width="256" height="128"/>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="75">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="89">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="90">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="91">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="92">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="107">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="108">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Corrupt_tree" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="4" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="20" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="32" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="33" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="34" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Normal_tree" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="48" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="49" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="50" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="51" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="52" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="64" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="65" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="66" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="67" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="68" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="80" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="81" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="82" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
